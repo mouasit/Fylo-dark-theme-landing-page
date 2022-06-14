418fcb8fc16db997f8ec2b9a6cfaa889c5d3580e
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-06-15 00:40:12 +0100

    active state

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const error = document.getElementById("error");
+const submit = document.getElementById("submit");
+
+var display = false;
+
+input.value = "";
+
+form.addEventListener("submit",function(e){
+    if ( input.value.match( /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
+    {
+        error.style.display = "none";
+        display = false;
+
+    }
+    else{
+        e.preventDefault();
+        {
+            error.style.display = "flex";
+            display = true;
+        }
+    }
+});
+
+input.addEventListener("input", function(){
+    if(display)
+    {
+        error.style.display = "none";
+        display = false;
+
+    }
+})
